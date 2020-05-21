334e0cf7a6f7d82b96cbbf44475a5a1225193e04
John <omnisonic@gmail.com>
2020-05-21 13:31:13 -0700

    made bars horizontal and cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,56 +1,30 @@
-console.log("hello JS chart world");
-// document.querySelector
-
-// document.createElement
 
 let apiData = {};
-console.log('global obj:', apiData);
-
 
 function render() {
-    // console.log('render getting invoked');
-    // let output = document.querySelector('#data');
-    // console.log('here is the output:', output);
-    // output.innerHTML = '';
 
-    for (var key in apiData) {
-        if (apiData.hasOwnProperty(key)) {
-            console.log(key + " -> " + apiData[key]);
+
+    for (var rateData in apiData) {
+        if (apiData.hasOwnProperty(rateData)) {
+            console.log(rateData + " -> " + apiData[rateData]);
             var div = document.createElement("div")
-            div.innerHTML = key;
-            x = apiData[key] + 'px';
+            div.innerHTML = rateData;
+            barSize = apiData[rateData] + 'px';
             document.getElementById("data").appendChild(div).style.
-            height = x;
+            width = barSize;
         }
-        // let rate = document.createElement('div');
-        // rate.setAttribute('src', gif.images.preview_gif.url);
-        // output.appendChild(rate);
-
-        // An example of adding a click event:
-        // image.onclick = () => alert(`Titled: ${gif.title}`);
     }
 }
-// render()
 
 function doFetch() {
     console.log('-- doFetch is getting invoked');
-    // let element = document.querySelector('#data');
-    // let searchTerm = element.value;
+
     fetch('https://api.exchangeratesapi.io/latest')
         .then(response => response.json())
         .then(jsonData => {
             let RateObj = jsonData.rates;
-            console.log('arr', jsonData);
             apiData = RateObj;
             render();
         });
 }
 doFetch();
-
-
-
-
-// snippent form stackoverflow: create and style div using Javasctript
-// var div = document.createElement("div");
-// div.innerHTML = "Hello";
-// document.getElementById("main").appendChild(div);
